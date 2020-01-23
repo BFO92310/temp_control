9cd33f6a2f7fc785972b8d0faf717e944c925531
BFO92310 <b.fortune@esc-montpellier.com>
2020-01-24 00:41:17 +0100

    Update temp_control.py

--- temp_control.py
+++ temp_control.py
@@ -1,8 +1,13 @@
 import Adafruit_DHT
+import GPi.GPIO as GPIO
+
+GPIO.setwarnings(False)
 sensor = Adafruit_DHT.DHT11
 DHT11_pin = 12
 
+
 humidity, temperature = Adafruit_DHT.read_retry(sensor, DHT11_pin)
+print(GPIO.input(DHT_pin))
 if humidity is not None and temperature is not None:
   print('Temperature={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
 elif humidity is not None and temperature is None:
