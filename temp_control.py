4ca43455ca23ba12708c4e1f155c64a1cf2776bf
BFO92310 <b.fortune@esc-montpellier.com>
2020-01-23 23:34:59 +0100

    Update temp_control.py

--- temp_control.py
+++ temp_control.py
@@ -5,5 +5,9 @@ DHT11_pin = 7
 humidity, temperature = Adafruit_DHT.read_retry(sensor, DHT11_pin)
 if humidity is not None and temperature is not None:
   print('Temperature={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+elif humidity is not None and temperature is None:
+  print('Pas de temperature and  Humidity={1:0.1f}%'.format(humidity))
+elif humidity is None and temperature is not None:
+  print('Temperature={0:0.1f}*C  No humidity'.format(temperature))
 else:
   print('Failed to get reading from the sensor. Try again!')
